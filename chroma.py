a4a662649494ca9c303a5eb4cddbd6a9297cb000
Thibault <thibault@manty.eu>
2025-06-30 16:14:31 +0200

    rm chroma db

--- chroma.py
+++ chroma.py
@@ -4,11 +4,11 @@ from langchain.vectorstores import Chroma
 def populate(chunks, embeddings):
     # Crée et persiste le vecteur
     vectorstore = Chroma.from_documents(
-        chunks, embeddings, persist_directory="notion_chroma"
+        chunks, embeddings, persist_directory="website_chroma"
     )
     vectorstore.persist()
 
     vectorstore = Chroma(
-        persist_directory="notion_chroma", embedding_function=embeddings
+        persist_directory="website_chroma", embedding_function=embeddings
     )
     return vectorstore
